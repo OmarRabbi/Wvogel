7fac875e5e156cdeac500056163b55d46ac146fb
OmarRabbi <omarfaruquerabbi0@gmail.com>
2025-02-24 16:27:55 +0600

    sidebar created for dashboard

--- src/components/dashboard/sidebar.jsx
+++ src/components/dashboard/sidebar.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
+import { Link, NavLink } from "react-router";
+import profileIcon from '@/assets/icons/profile-icon.svg';
+import chatIcon from '@/assets/icons/chat-icon.svg';
+import logoutIcon from '@/assets/icons/logout-icon.svg';
+import notificationIcon from '@/assets/icons/notification-icon.svg';
+import { useLocation } from 'react-router';
 
 function Sidebar() {
-  return (
-    <div className='w-full p-4 sm:p-6 lg:p-8'>
-        Sidebar
-    </div>
-  )
+    const { pathname } = useLocation()
+    const sidebars = [
+        {
+            label: 'My Profile',
+            icon: profileIcon,
+            path: '/dashboard',
+        },
+        {
+            label: 'Chat',
+            icon: chatIcon,
+            path: '/dashboard/chat',
+        },
+        {
+            label: 'Notification',
+            icon: notificationIcon,
+            path: '/dashboard/notification',
+        },
+        {
+            label: 'Logout',
+            icon: logoutIcon,
+            path: '/auth/logout',
+        },
+    ]
+
+
+    return (
+        <div className='w-full p-4 sm:p-6 lg:p-8'>
+            <div className='flex flex-col gap-2'>
+                {
+                    sidebars.map((sidebar, index) => (
+                        <NavLink
+                            key={index}
+                            to={sidebar.path}
+                            end
+                            className={({ isActive }) => `flex items-center gap-3 px-4 py-3 rounded-[4px]  ${isActive ? 'bg-[#E52B4233] border-[#E52B42]' : ''}`}>
+                            <img src={sidebar.icon} alt="icon" />
+                            <p className='text-base font-medium leading-6 text-header-foreground'>{sidebar.label}</p>
+                        </NavLink>
+                    ))}
+            </div>
+        </div>
+    )
 }
 
 export default Sidebar
+
+// <Link
+//     key={index}
+//     to={sidebar.path}
+//     className={`flex items-center gap-3 px-4 py-3 rounded-[4px] border border-black ${pathname === sidebar.path ? 'bg-[#E52B4233] border-[#E52B42]' : ''}`}>
+//         <img src={sidebar.icon} alt="icon" />
+//     <p>{sidebar.label}</p>
+// </Link>
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,31 +3,42 @@ import Home from "@/pages/main/home";
 import { createBrowserRouter } from "react-router";
 import DashboardLayout from "@/layouts/dashboard-layout";
 import MainLayout from "@/layouts/main-layout";
+import Profile from "@/pages/dashboard/profile";
+import Chat from "@/pages/dashboard/chat";
+import Notification from "@/pages/dashboard/notification";
 
 export const router = createBrowserRouter([
   {
     path: "/dashboard",
     element: <DashboardLayout />,
     children: [
-      { 
-        path: "", 
-        element: <h1>Dashboard</h1>,
+      {
+        index: true,
+        element: <Profile />,
+      },
+      {
+        path: 'chat',
+        element: <Chat />,
+      },
+      {
+        path: 'notification',
+        element: <Notification />,
       },
     ],
   },
-    {
-        path: '/',
-        element: <MainLayout/>,
-        children: [
-          {
-            index: true,
-            element: <Home/>,
-          },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
 
-        ]
-    },
-    {
-        path: '/sign-in',
-        element: <SignIn/>,
-    }
+    ]
+  },
+  {
+    path: '/sign-in',
+    element: <SignIn />,
+  }
 ])

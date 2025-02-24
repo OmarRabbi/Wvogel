72c08d50b38509b96206e673c53f3de6d6951c6f
OmarRabbi <omarfaruquerabbi0@gmail.com>
2025-02-24 11:18:26 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+      Hello world!
+    </h1>
+    <Button className="" variant='secondary'>Submit</Button>
+    </>
+  )
+}
+
+export default App
